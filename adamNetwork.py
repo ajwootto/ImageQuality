7f95bd97b1489f738a05ad78b37626f540d3b632
Adam Wootton <ajwootto@uwaterloo.ca>
2016-04-19 21:11:30 -0400

    add model initialization for lenet and vgg

--- adamNetwork.py
+++ adamNetwork.py
@@ -1,11 +1,12 @@
 from keras.models import Sequential
 from keras.models import model_from_json
 
-from keras.layers.core import Dense, Activation, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
 from keras.utils import np_utils
 from scipy import misc
+from keras.regularizers import l2, activity_l2
 
 import scipy.io as sio
 import numpy as np
@@ -46,18 +47,19 @@ image_names = massage_mat_array(image_names)
 image_names, image_scores = shuffle_in_unison(image_names, image_scores)
 
 #define zero arrays for training and testing data
-X_train = np.zeros((num_train_samples, img_dim, img_dim, 3), dtype='uint8')
+X_train = np.zeros((num_train_samples, 3, img_dim, img_dim), dtype='uint8')
 Y_train = np.zeros((num_train_samples,), dtype='uint8')
 
-X_test = np.zeros((num_test_samples, img_dim, img_dim, 3), dtype='uint8')
+X_test = np.zeros((num_test_samples, 3, img_dim, img_dim), dtype='uint8')
 y_test = np.zeros((num_test_samples,), dtype='uint8')
 
 #load training images into memory
 for i in range(0, num_train_samples):
   img = misc.imread('Images/' + image_names[i])
   img = misc.imresize(img, (img_dim, img_dim))
+  img = img.transpose((2,0,1))
   X_train[i, :, :, :] = img
-  Y_train[i] = 1 if (image_scores[i] > 50) else 0
+  Y_train[i] = image_scores[i] #1 if (image_scores[i] > 50) else 0
 
 #load testing images into memory
 for i in range(0, num_test_samples):
@@ -65,37 +67,39 @@ for i in range(0, num_test_samples):
   test_index = i + num_train_samples
   img = misc.imread('Images/' + image_names[test_index])
   img = misc.imresize(img, (img_dim, img_dim))
+  img = img.transpose((2,0,1))
   X_test[i, :, :, :] = img
-  y_test[i] = 1 if (image_scores[test_index] > 50) else 0
+  y_test[i] = image_scores[test_index] #1 if (image_scores[test_index] > 50) else 0
 
 Y_train = np.reshape(Y_train, (len(Y_train), 1))
 y_test = np.reshape(y_test, (len(y_test), 1))
 
-
 #convert to categorical data type (not sure what this does yet but it was in the example)
-#import ipdb; ipdb.set_trace()
-Y_train = np_utils.to_categorical(Y_train, num_classes)
-Y_test = np_utils.to_categorical(y_test, num_classes)
-
-#create new model
-model = Sequential()
-
-#add layers
-#dim ordering parameter sets channel as the third dimension instead of the first (mirroring the way imread works)
-model.add(Convolution2D(64, 3, 3, border_mode='same', input_shape=(img_dim, img_dim, 3), dim_ordering="tf"))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Dense(output_dim=num_classes))
-model.add(Activation("softmax"))
-
-
+#Y_train = np_utils.to_categorical(Y_train, num_classes)
+#Y_test = np_utils.to_categorical(y_test, num_classes)
+Y_test = y_test
 #normalize image data to be between 0 and 1
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 X_train /= 255
 X_test /= 255
+
+
+def initialize_model():
+  #create new model
+  model = Sequential()
+
+  #add layers
+  #dim ordering parameter sets channel as the third dimension instead of the first (mirroring the way imread works)
+  model.add(Convolution2D(64, 3, 3, border_mode='same', input_shape=(img_dim, img_dim, 3), dim_ordering="tf"))
+  model.add(Activation('relu'))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Flatten())
+  model.add(Dense(512))
+  model.add(Dense(output_dim=num_classes))
+  model.add(Activation("softmax"))
+
+  return model
 
 def train_model(model):
   #define optimizer with learning rate, momentum etc.
@@ -104,8 +108,8 @@ def train_model(model):
   model.compile(loss='mean_squared_error', optimizer='rmsprop')
   #train model
   model.fit(X_train, Y_train,
-                batch_size=100,
-                nb_epoch=100,
+                batch_size=5,
+                nb_epoch=200,
                 validation_data=(X_test, Y_test),
                 shuffle=True)
   return model
@@ -127,11 +131,102 @@ def load_model():
   loaded_model.load_weights('weights')
   return loaded_model
 
-model = load_model()
+#initializes a model based on the VGG architecture which placed second in 2014 imagenet competition
+def initialize_vgg():
+  model = Sequential()
+  model.add(ZeroPadding2D((1,1), input_shape=(3,224,224)))
+  model.add(Convolution2D(64, 3, 3, activation='relu'))
+  model.add(ZeroPadding2D((1,1)))
+  model.add(Convolution2D(64, 3, 3, activation='relu'))
+  model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+  model.add(ZeroPadding2D((1,1)))
+  model.add(Convolution2D(128, 3, 3, activation='relu'))
+  model.add(ZeroPadding2D((1,1)))
+  model.add(Convolution2D(128, 3, 3, activation='relu'))
+  model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+  model.add(ZeroPadding2D((1,1)))
+  model.add(Convolution2D(256, 3, 3, activation='relu'))
+  model.add(ZeroPadding2D((1,1)))
+  model.add(Convolution2D(256, 3, 3, activation='relu'))
+  model.add(ZeroPadding2D((1,1)))
+  model.add(Convolution2D(256, 3, 3, activation='relu'))
+  model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+  model.add(ZeroPadding2D((1,1)))
+  model.add(Convolution2D(512, 3, 3, activation='relu'))
+  model.add(ZeroPadding2D((1,1)))
+  model.add(Convolution2D(512, 3, 3, activation='relu'))
+  model.add(ZeroPadding2D((1,1)))
+  model.add(Convolution2D(512, 3, 3, activation='relu'))
+  model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+  model.add(ZeroPadding2D((1,1)))
+  model.add(Convolution2D(512, 3, 3, activation='relu'))
+  model.add(ZeroPadding2D((1,1)))
+  model.add(Convolution2D(512, 3, 3, activation='relu'))
+  model.add(ZeroPadding2D((1,1)))
+  model.add(Convolution2D(512, 3, 3, activation='relu'))
+  model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+  model.add(Flatten())
+  model.add(Dense(4096, activation='relu'))
+  model.add(Dropout(0.5))
+  model.add(Dense(4096, activation='relu'))
+  model.add(Dropout(0.5))
+  model.add(Dense(1, activation='linear'))
+
+  return model
+
+
+#initialize a model based on the LeNet-5 architecture (1995)
+def initialize_lenet():
+  model = Sequential()
+ 
+  # declare the first layers of convolution and pooling
+  #
+  num_filters = 6
+  # side length of maxpooling square
+  num_pool = 2
+  # side length of convolution square
+  num_conv = 5
+
+  model.add(Convolution2D(6, num_conv, num_conv, border_mode='same', input_shape=(3, img_dim, img_dim)))
+  model.add(Activation('relu'))
+  #model.add(Convolution2D(num_filters, num_conv, num_conv))
+  #model.add(Activation('relu'))
+  model.add(MaxPooling2D((num_pool,num_pool)))
+  model.add(Dropout(0.5))
+   
+  model.add(Convolution2D(16, num_conv, num_conv, border_mode='same' ))
+  model.add(Activation('relu'))
+  #model.add(Convolution2D(num_filters, num_conv, num_conv))
+  #model.add(Activation('relu'))
+  model.add(MaxPooling2D((num_pool,num_pool)))
+  model.add(Dropout(0.5))
+  
+  model.add(Convolution2D(120, num_conv, num_conv))
+  model.add(Activation('relu'))
+
+  model.add(Flatten())
+  model.add(Dense(84))  
+                                   
+  model.add(Activation('relu'))
+  model.add(Dropout(0.5))           
+   
+  model.add(Dense(1, W_regularizer=l2(0.01) ))
+  model.add(Activation('linear') )
+
+  return model
+
+model = initialize_lenet()
+model = train_model(model)
+save_model(model)
 
 #output predicted classes of test data
-predictions = model.predict_classes(X_test, batch_size=3, verbose=1)
-
+predictions = model.predict_proba(X_test, batch_size=3, verbose=1)
+print predictions
 misclassified = np.sum(np.absolute(np.subtract(predictions, y_test.flatten())))
 
 print misclassified
